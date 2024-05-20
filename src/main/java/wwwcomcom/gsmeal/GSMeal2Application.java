192e3dcde12f8b60c4a4d82eae60e05b665bb5be
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-05-20 19:04:35 +0900

    initialize spring boot project

---
+++ src/main/java/wwwcomcom/gsmeal/GSMeal2Application.java
@@ -0,0 +1,13 @@
+package wwwcomcom.gsmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GSMeal2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GSMeal2Application.class, args);
+    }
+
+}
---
+++ src/test/java/wwwcomcom/gsmeal/GSMeal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package wwwcomcom.gsmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GSMeal2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
